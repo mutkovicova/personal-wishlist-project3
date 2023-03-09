1dcc14c1c0d1dab0a6278703be73c9c65424b1c3
mutkovicova <slovakgal@gmail.com>
2023-03-09 17:06:05 +0000

    fix select

---
+++ wishlist/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+});
